01e297176747a56605a522b7994308fdd16fcd71
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-09-13 23:29:48 +0600

    auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;use App\Http\Controllers\Api\V1\CompletedTaskController;
 
 
-Route::prefix('v1')->group(function (){
+Route::middleware('auth:sanctum')->prefix('v1')->group(function (){
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompletedTaskController::class);
 });
