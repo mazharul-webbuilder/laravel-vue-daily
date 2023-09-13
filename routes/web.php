fac1a1db3591c21d3248f1015a1cff9b1c00ce14
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-09-13 23:10:43 +0600

    api auth registration using sanctum

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\Auth\LoginController;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,4 +12,6 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function (){
    Route::post('/login', LoginController::class);
+   Route::post('/logout', LogoutController::class);
+   Route::post('/register', RegisterController::class);
 });
