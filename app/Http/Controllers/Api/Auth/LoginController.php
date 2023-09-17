b5a64460ba6aee67669a61301abf46d1b9503454
irozen-mazharul <141237975+irozen-mazharul@users.noreply.github.com>
2023-09-17 15:56:12 +0600

    Token based authentication using sanctum

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'message' => ['The credential you entered is invalid']
+            ]);
+        }
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('laravel_api_token')->plainTextToken
+        ]);
+    }
+}
+
+//ADD Below lines to POSTMan request Pre-request-script of request for SPA API Authentication
+//OOtherwise you will get csrf token mismatch error
+
+
+//pm.sendRequest({
+//    url: "http://127.0.0.1:8000/sanctum/csrf-cookie",
+//    method: "GET",
+//}, function(err, res, {cookies}){
+//    if(!err) {
+//        pm.globals.set('csrf-token', cookies.get('XSRF-TOKEN'))
+//}
+//})
+
+
+//ADD this to reqeust header
+//Accept = application/json
+//X-XSRF-TOKEN = {{csrf-token}}  // the csrf-token is comes from global that you created by pre script
+
+// One more think to get valid api response as SPA pass in header Referer = localhost (You domain name)
+
+
+// Your SPA Santum API reqeust will authorized
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 
 use App\Http\Controllers\Api\V1\TaskController;use App\Http\Controllers\Api\V1\CompletedTaskController;
 
@@ -9,6 +12,14 @@ use App\Http\Controllers\Api\V1\TaskController;use App\Http\Controllers\Api\V1\C
 Route::middleware('auth:sanctum')->prefix('v1')->group(function (){
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('tasks/{task}/completed', CompletedTaskController::class);
+});
+
+
+/*Sanctum API Token Authentication*/
+Route::prefix('auth')->group(function (){
+    Route::post('/login', LoginController::class)->middleware('guest');
+    Route::post('/register', RegisterController::class)->middleware('guest');
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
 });
 
 
