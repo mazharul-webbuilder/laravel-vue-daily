8add764a9926c1a99b47ff6a09a57076241a4aa3
irozen-mazharul <141237975+irozen-mazharul@users.noreply.github.com>
2023-09-19 13:33:25 +0600

    dynamicly vue completed and uncompleted task

--- vue/toe-doe/src/components/tasks/Task.vue
+++ vue/toe-doe/src/components/tasks/Task.vue
@@ -1,8 +1,8 @@
 <template>
     <li class="list-group-item py-3" >
         <div class="d-flex justify-content-start align-items-center">
-            <input class="form-check-input mt-0 completed" type="checkbox" />
-            <div class="ms-2 flex-grow-1" title="Double click the text to edit or remove">
+            <input class="form-check-input mt-0 completed" :class="completedClass" type="checkbox" :checked="task.is_completed" />
+            <div class="ms-2 flex-grow-1" :class="completedClass" title="Double click the text to edit or remove">
                 <!-- <div class="relative">
                     <input class="editable-task" type="text" />
                 </div> -->
@@ -23,7 +23,12 @@
 <script setup>
 import IconPencil from "@/components/icons/IconPencil.vue";
 import TrashIcon from "@/components/icons/TrashIcon.vue";
-defineProps({
+import {computed} from "vue";
+const props = defineProps({
     task: Object
 })
+
+const completedClass = computed(() => props.task.is_completed ? "completed" : "")
+
+
 </script>
